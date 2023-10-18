c4096f13ae548a0e8b2ad36ef94c0c977df666ec
Kejdzej18 <144226418+Kejdzej18@users.noreply.github.com>
2023-10-18 12:21:19 +0200

    Add files via upload
    18.10

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-//function Person(par1, par2, par3){
-
-//}
-
-class Person{
-    constructor(firstName, lastName, birthDate){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.birthDate=birthDate;
-    }
-    getFullName(){
-        return {
-            firstName: this.firstName,
-            lastName: this.lastName
+window.addEventListener("DOMContentLoaded", ()=>{
+    const form = document.forms[0];
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        //let inputs = form.querySelectorAll("input[type=\"text\"]");
+        let inputs = form.elements;
+        for(let i = 0; i< inputs.length;i++){
+            //(inputs[i].type !== "submit" && inputs[i].type !== "reset" && inputs[i].type !== "button") ? console.log(inputs[i].value) : (null);
+            document.querySelector("body").style.backgroundColor = inputs[1].value;
         };
-    }
-    getAge(){
-        let age = (new Date()).getFullYear() - this.birthDate;
-        return { 
-            ...this.getFullName(),
-            age: age
-        }
-    }
-}
-const adam = new Person("Adam", "Nowak", 1958);
-const jan = new Person("Jan", "Kowalski", 1969);
-Person.prototype.weight = undefined;
-Person.prototype.getWeight = function(){
-    return {
-        ...this.getFullName,
-        weight: this.weight
-    }
-}
-adam.weight = 69;
-jan.weight = 145;
-const tab = [adam, jan];
-
-for(let i = 0; i < tab.length; i++){
-    tab[i].getAge()
-}
-tab.forEach(el=>{
-    el.getAge()
+    })
 });
