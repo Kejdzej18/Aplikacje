2808dac05602d588604dfdbaa794137f2c5b8f69
Kejdzej18 <144226418+Kejdzej18@users.noreply.github.com>
2023-10-25 12:24:21 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,7 +1,45 @@
+class Shiritori {
+    constructor() {
+        this.words = [];
+    }
+    play(word) {
+        let check = this.check(word);
+        if(check.check) {
+            this.words.push(word);
+            return { check: check.check, message: 'Słowo dodane do tablicy' };
+        }
+        else return { check: check.check, message: 'Gra zakończona! ' + check.reason };
+    }
+    check(word) {
+        if(this.words.indexOf(word) == -1) {
+            let lastWord;
+            if(this.words.length >= 1) {
+                lastWord = this.words[this.words.length - 1];
+                let lastSign = lastWord[lastWord.length - 1];
+                let firstSign = word[0];
+                if(lastSign == firstSign) return { check: true };
+                return { check: false, reason: 'Słowo "' + word + '" nie rozpoczyna się od litery ' + firstSign + '!' };
+            }
+            return { check: true };
+        }
+        return { check: false, reason: 'Słowo "' + word + '" już było użyte!' };
+    }
+    restart() {
+        this.words = [];
+    }
+    getWords() {
+        return this.words;
+    }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-    let button = document.querySelector("#chHeader");
-    button.addEventListener("click", () => {
-        let headerToCh = document.querySelector("#headerToCh");
-        headerToCh.innerHTML = "Ala ma kota";
+    const shiritori = new Shiritori();
+    let form = document.forms[0];
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let play = shiritori.play(form.word.value);
+        form.word.value = '';
+        document.querySelector('#message').innerHTML = play.message;
+        document.querySelector('#words').innerHTML = shiritori.words;
     });
 });
