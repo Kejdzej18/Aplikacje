18e93621b326af15a3aa7387c2f81fb1f8a91ae8
Kejdzej18 <144226418+Kejdzej18@users.noreply.github.com>
2023-11-06 11:23:13 +0100

    Add files via upload

--- script31.js
+++ script31.js
@@ -1,30 +1,31 @@
-function wzorHerona(a, b, c) {
-    if((a > 0 && b > 0 && c > 0) && (a + b >= c) && (a + c >= b) && (b + c >= a)) {
-        let p = ((a) + (b) + (c)) / 2;
-        return Math.round(Math.sqrt(p * (p - a) * (p - b) * (p - c)));
-    }
-    else return false;
-}
-
 window.addEventListener("DOMContentLoaded", () => {
-    let bok1 = document.querySelector("#bok-1");
-    let bok2 = document.querySelector("#bok-2");
-    let bok3 = document.querySelector("#bok-3");
-    let form = document.forms[0];
-    document.querySelector("#bokData-1").addEventListener("click", () => {
-        bok1.value = prompt("Wprowadź wartość");
-    });
-    document.querySelector("#bokData-2").addEventListener("click", () => {
-        bok2.value = prompt("Wprowadź wartość");
-    });
-    document.querySelector("#bokData-3").addEventListener("click", () => {
-        bok3.value = prompt("Wprowadź wartość");
-    });
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let b1 = parseInt(bok1.value);
-        let b2 = parseInt(bok2.value);
-        let b3 = parseInt(bok3.value);
-        document.querySelector("#wynik").innerHTML = wzorHerona(b1, b2, b3);
-    });
+    if ("content" in document.createElement("template")) {
+        const tbody = document.querySelector("#form-tbody");
+        const template = document.querySelector("#userrow");
+        const form = document.forms[0];
+        document.querySelector("#addRow").addEventListener("click", () => {
+            let clone = template.content.cloneNode(true);
+            tbody.appendChild(clone);
+        });
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            let rows = tbody.querySelectorAll("tr");
+            let dataObject = {};
+            let i = 0;
+            rows.forEach(element => {
+                let inputs = element.querySelectorAll("input");
+                let obj = {
+                    "input-1": inputs[0].value,
+                    "input-2": inputs[1].value,
+                    "input-3": inputs[2].value
+                }
+                dataObject[i] = obj;
+                i++;
+            });
+            console.log(dataObject);
+        });
+    }
+    else {
+        alert("Dzban");
+    }
 });
